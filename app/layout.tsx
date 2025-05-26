52f10edea78a42b76b534bc0a82b1dc8d1e3d795
Kevin Grisales <kgrisales64@gmail.com>
2025-05-26 12:16:29 -0500

    fix: Actualizar enlaces en la sección 5.5 del curso de Git

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <Banner variant="rainbow" id="bienvenida-aprendiz-adso">
-          ¡Hola aprendiz ADSO! Este espacio fue creado para acompañarte en tu camino como desarrollador. Descubre contenido útil, claro y práctico.
+        <Banner variant="rainbow" id="bienvenida-aprendiz">
+          ¡Hola aprendiz! Este espacio fue creado para acompañarte en tu camino como desarrollador. Descubre contenido útil, claro y práctico.
         </Banner>
         <RootProvider>{children}</RootProvider>
       </body>
